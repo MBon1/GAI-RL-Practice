31e2f0ade5a967f7170fb65b16c09d4fdbbdecda
grishhung <justinhung@gmail.com>
2023-03-29 22:21:08 -0400

    Abandon ship -- it's maze time

--- GAI-RL-Practice/Assets/Challenge 1/Scripts/QLAgent.cs
+++ GAI-RL-Practice/Assets/Challenge 1/Scripts/QLAgent.cs
@@ -4,198 +4,198 @@ using UnityEngine;
 
 public class QLAgent : MonoBehaviour
 {
-    // Stuff from the original code
-    public float speed = 15.0f;
-    public float rotationSpeed = 100.0f;
+    // // Stuff from the original code
+    // public float speed = 15.0f;
+    // public float rotationSpeed = 100.0f;
 
-    // Q-learning parameters
-    public float learningRate = 0.1f;
-    public float discountFactor = 0.99f;
-    public float explorationRate = 0.1f;
+    // // Q-learning parameters
+    // public float learningRate = 0.1f;
+    // public float discountFactor = 0.99f;
+    // public float explorationRate = 0.1f;
 
-    // State space
-    private Vector3 position;
-    private Vector3 velocity;
-    private float orientation;
+    // // State space
+    // private Vector3 position;
+    // private Vector3 velocity;
+    // private float orientation;
 
-    // Action space
-    private float[] actions = {-1.0f, 0.0f, 1.0f};
+    // // Action space
+    // private float[] actions = {-1.0f, 0.0f, 1.0f};
 
-    // Q-values
-    private Dictionary<string, Dictionary<float, float>> qTable = new Dictionary<string, Dictionary<float, float>>();
+    // // Q-values
+    // private Dictionary<string, Dictionary<float, float>> qTable = new Dictionary<string, Dictionary<float, float>>();
 
-    // Raycasting parameters
-    public float raycastDistance = 1.0f;  /* ASSIGNMENT: Figure out what this is supposed to be */
-    public LayerMask obstacleLayer;
+    // // Raycasting parameters
+    // public float raycastDistance = 1.0f;  /* ASSIGNMENT: Figure out what this is supposed to be */
+    // public LayerMask obstacleLayer;
 
-    // Reward function
-    private float GetReward()
-    {
-        /* ASSIGNMENT: Make this work properly with more than one straight ray */
+    // // Reward function
+    // private float GetReward()
+    // {
+    //     /* ASSIGNMENT: Make this work properly with more than one straight ray */
 
-        /* NOTE: It's doing this based on the ray, not the collision, which is weird, but it actually makes sense */
-        /*       In fact, it could stand to reason that we should instead get the position of the hit and assign  */
-        /*       rewards based on how close the hit is to the center of the gap. That might work even better.     */
+    //     /* NOTE: It's doing this based on the ray, not the collision, which is weird, but it actually makes sense */
+    //     /*       In fact, it could stand to reason that we should instead get the position of the hit and assign  */
+    //     /*       rewards based on how close the hit is to the center of the gap. That might work even better.     */
 
-        RaycastHit hit;
+    //     RaycastHit hit;
 
-        bool targetHit = Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance, targetLayer);
-        bool wallHit = Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance, wallLayer);
+    //     bool targetHit = Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance, targetLayer);
+    //     bool wallHit = Physics.Raycast(transform.position, transform.forward, out hit, raycastDistance, wallLayer);
         
-        if (targetHit)
-        {
-            if (hit.collider.CompareTag("target"))
-            {
-                // The ray hit a gap
-                return 1.0f;
-            }
-        }
-        else if (wallHit)
-        {
-            if (hit.collider.CompareTag("wall"))
-            {
-                // The ray hit an obstacle
-                return -1.0f;
-            }
-        }
+    //     if (targetHit)
+    //     {
+    //         if (hit.collider.CompareTag("target"))
+    //         {
+    //             // The ray hit a gap
+    //             return 1.0f;
+    //         }
+    //     }
+    //     else if (wallHit)
+    //     {
+    //         if (hit.collider.CompareTag("wall"))
+    //         {
+    //             // The ray hit an obstacle
+    //             return -1.0f;
+    //         }
+    //     }
 
-        // NOTE: if this has happened, something has probably gone wrong and it is likely looking over or under the wall
+    //     // NOTE: if this has happened, something has probably gone wrong and it is likely looking over or under the wall
+
+    //     // Nothing happened; default reward
+    //     return 0.0f;
+    // }
+
+    // // Convert state to string for use as dictionary key
+    // private string StateToString(Vector3 position, Vector3 velocity, float orientation)
+    // {
+    //     /* ASSIGNMENT: Truncate the float vals somehow because floating point equivalence is dog shit */
+    //     /*             Maybe make it one decimal point or something to match approximate equivalence  */
+
+    //     return position.ToString() + velocity.ToString() + orientation.ToString();
+    // }
+
+    // // Get highest Q-value for a state
+    // private float MaxQ(string state)
+    // {
+    //     float maxQ = float.MinValue;
+
+    //     if (qTable.ContainsKey(state))
+    //     {
+    //         foreach (float action in actions)
+    //         {
+    //             if (qTable[state][action] > maxQ)
+    //             {
+    //                 maxQ = qTable[state][action];
+    //             }
+    //         }
+    //     }
+
+    //     return maxQ;
+    // }
+
+    // // Get action with highest Q-value for a state
+    // private float MaxQAction(string state)
+    // {
+    //     float maxQ = float.MinValue;
+    //     float maxAction = 0.0f;
+
+    //     if (qTable.ContainsKey(state))
+    //     {
+    //         foreach (float action in actions)
+    //         {
+    //             if (qTable[state][action] > maxQ)
+    //             {
+    //                 maxQ = qTable[state][action];
+    //                 maxAction = action;
+    //             }
+    //         }
+    //     }
+
+    //     return maxAction;
+    // }
+
+    // // Get Q-value for a state-action pair
+    // private float GetQValue(Vector3 position, Vector3 velocity, float orientation, float action)
+    // {
+    //     string state = StateToString(position, velocity, orientation);
+
+    //     if (qTable.ContainsKey(state) && qTable[state].ContainsKey(action))
+    //     {
+    //         return qTable[state][action];
+    //     }
         
-        // Nothing happened; default reward
-        return 0.0f;
-    }
+    //     // The default Q-value
+    //     return 0.0f;
+    // }
 
-    // Convert state to string for use as dictionary key
-    private string StateToString(Vector3 position, Vector3 velocity, float orientation)
-    {
-        /* ASSIGNMENT: Truncate the float vals somehow because floating point equivalence is dog shit */
-        /*             Maybe make it one decimal point or something to match approximate equivalence  */
+    // // Set Q-value for a state-action pair
+    // private void SetQValue(Vector3 position, Vector3 velocity, float orientation, float action, float value)
+    // {
+    //     string state = StateToString(position, velocity, orientation);
 
-        return position.ToString() + velocity.ToString() + orientation.ToString();
-    }
+    //     if (!qTable.ContainsKey(state))
+    //     {
+    //         qTable[state] = new Dictionary<float, float>();
+    //     }
 
-    // Get highest Q-value for a state
-    private float MaxQ(string state)
-    {
-        float maxQ = float.MinValue;
+    //     qTable[state][action] = value;
+    // }
 
-        if (qTable.ContainsKey(state))
-        {
-            foreach (float action in actions)
-            {
-                if (qTable[state][action] > maxQ)
-                {
-                    maxQ = qTable[state][action];
-                }
-            }
-        }
+    // // Q-learning update
+    // private void QLearningUpdate(float action, float reward, Vector3 newPosition, Vector3 newVelocity, float newOrientation)
+    // {
+    //     string state = StateToString(newPosition, newVelocity, newOrientation);
 
-        return maxQ;
-    }
+    //     float maxQ = MaxQ(state);
+    //     float oldQ = GetQValue(position, velocity, orientation, action);
+    //     float newQ = (1.0f - learningRate) * oldQ + learningRate * (reward + discountFactor * maxQ);
 
-    // Get action with highest Q-value for a state
-    private float MaxQAction(string state)
-    {
-        float maxQ = float.MinValue;
-        float maxAction = 0.0f;
+    //     SetQValue(position, velocity, orientation, action, newQ);
+    // }
 
-        if (qTable.ContainsKey(state))
-        {
-            foreach (float action in actions)
-            {
-                if (qTable[state][action] > maxQ)
-                {
-                    maxQ = qTable[state][action];
-                    maxAction = action;
-                }
-            }
-        }
+    // // Choose an action based on the current state
+    // private float ChooseAction()
+    // {
+    //     if (Random.value < explorationRate)
+    //     {
+    //         return actions[Random.Range(0, actions.Length)];
+    //     }
 
-        return maxAction;
-    }
+    //     return MaxQAction(StateToString(position, velocity, orientation));
+    // }
 
-    // Get Q-value for a state-action pair
-    private float GetQValue(Vector3 position, Vector3 velocity, float orientation, float action)
-    {
-        string state = StateToString(position, velocity, orientation);
+    // // Update is called once per frame
+    // void FixedUpdate()
+    // {
+    //     // Save current state
+    //     Vector3 oldPosition = position;
+    //     Vector3 oldVelocity = velocity;
+    //     float oldOrientation = orientation;
 
-        if (qTable.ContainsKey(state) && qTable[state].ContainsKey(action))
-        {
-            return qTable[state][action];
-        }
-        
-        // The default Q-value
-        return 0.0f;
-    }
+    //     // Choose action
+    //     float action = ChooseAction();
 
-    // Set Q-value for a state-action pair
-    private void SetQValue(Vector3 position, Vector3 velocity, float orientation, float action, float value)
-    {
-        string state = StateToString(position, velocity, orientation);
+    //     // Update position and orientation based on action
+    //     transform.Rotate(-action * Vector3.right * rotationSpeed * Time.deltaTime);
+    //     transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
-        if (!qTable.ContainsKey(state))
-        {
-            qTable[state] = new Dictionary<float, float>();
-        }
+    //     // Get new state
+    //     position = transform.position;
+    //     velocity = (position - oldPosition) / Time.deltaTime;
+    //     orientation = transform.eulerAngles.x;
 
-        qTable[state][action] = value;
-    }
+    //     // Get reward
+    //     float reward = GetReward();
 
-    // Q-learning update
-    private void QLearningUpdate(float action, float reward, Vector3 newPosition, Vector3 newVelocity, float newOrientation)
-    {
-        string state = StateToString(newPosition, newVelocity, newOrientation);
+    //     // Update Q-values
+    //     QLearningUpdate(action, reward, position, velocity, orientation);
 
-        float maxQ = MaxQ(state);
-        float oldQ = GetQValue(position, velocity, orientation, action);
-        float newQ = (1.0f - learningRate) * oldQ + learningRate * (reward + discountFactor * maxQ);
-
-        SetQValue(position, velocity, orientation, action, newQ);
-    }
-
-    // Choose an action based on the current state
-    private float ChooseAction()
-    {
-        if (Random.value < explorationRate)
-        {
-            return actions[Random.Range(0, actions.Length)];
-        }
-
-        return MaxQAction(StateToString(position, velocity, orientation));
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        // Save current state
-        Vector3 oldPosition = position;
-        Vector3 oldVelocity = velocity;
-        float oldOrientation = orientation;
-
-        // Choose action
-        float action = ChooseAction();
-
-        // Update position and orientation based on action
-        transform.Rotate(-action * Vector3.right * rotationSpeed * Time.deltaTime);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-
-        // Get new state
-        position = transform.position;
-        velocity = (position - oldPosition) / Time.deltaTime;
-        orientation = transform.eulerAngles.x;
-
-        // Get reward
-        float reward = GetReward();
-
-        // Update Q-values
-        QLearningUpdate(action, reward, position, velocity, orientation);
-
-        // Check for collision
-        if (reward == -1.0f) {
-            transform.position = oldPosition;
-            position = oldPosition;
-            velocity = oldVelocity;
-            orientation = oldOrientation;
-        }
-    }
+    //     // Check for collision
+    //     if (reward == -1.0f) {
+    //         transform.position = oldPosition;
+    //         position = oldPosition;
+    //         velocity = oldVelocity;
+    //         orientation = oldOrientation;
+    //     }
+    // }
 }
---
+++ GAI-RL-Practice/Assets/SimpleMaze/QLMaze.cs
@@ -0,0 +1,262 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QLMaze : MonoBehaviour
+{
+    // Q-learning variables
+    private Dictionary<Tuple<int, int>, Dictionary<string, float>> q_table;
+    private float learning_rate = 0.8f;
+    private float discount_factor = 0.95f;
+    private float exploration_rate = 0.2f;
+
+    // Maze variables
+    private int nx;
+    private int ny;
+    private Tuple<int, int> start_pos;
+    private Tuple<int, int> goal_pos;
+    private List<Tuple<int, int>> obstacles;
+
+    // Agent variables
+    private Tuple<int, int> current_pos;
+    private string[] actions = { "up", "down", "left", "right" };
+
+    // Unity variables
+    public GameObject agent;
+    public GameObject goal;
+    public GameObject[] obstacles_go;
+
+    // Timestep regulation variables
+    /* ASSIGNMENT: only call Update() x times per second */
+
+    // Episode regulation variables
+    int max_steps = 1000;
+    int curr_step = 0;
+
+    void Start()
+    {
+        // Initialize maze variables
+        nx = 10; // set to desired size
+        ny = 10; // set to desired size
+        start_pos = new Tuple<int, int>(0, 0); // set to desired start position
+        goal_pos = new Tuple<int, int>(nx - 1, ny - 1); // set to desired goal position
+        obstacles = new List<Tuple<int, int>>();
+        foreach (GameObject obstacle in obstacles_go)
+        {
+            obstacles.Add(new Tuple<int, int>(
+                (int)obstacle.transform.position.x,
+                (int)obstacle.transform.position.z));
+        }
+
+        // Initialize Q-learning variables
+        q_table = new Dictionary<Tuple<int, int>, Dictionary<string, float>>();
+        for (int i = 0; i < nx; i++)
+        {
+            for (int j = 0; j < ny; j++)
+            {
+                Tuple<int, int> state = new Tuple<int, int>(i, j);
+                Dictionary<string, float> state_actions = new Dictionary<string, float>();
+                foreach (string action in actions)
+                {
+                    state_actions.Add(action, 0.0f);
+                }
+                q_table.Add(state, state_actions);
+            }
+        }
+
+        // Initialize agent variables
+        current_pos = start_pos;
+    }
+
+    void Update()
+    {
+        // Update agent position and Q-learning table
+        Tuple<int, int> next_pos = ChooseAction();
+        float reward = GetReward(next_pos);
+        UpdateQTable(current_pos, next_pos, reward);
+        current_pos = next_pos;
+        agent.transform.position = new Vector3(current_pos.Item1 + 0.5f, 0.5f, current_pos.Item2 + 0.5f);
+
+        // Keep track of the current step
+        bool restart_needed = false;
+        curr_step++;
+
+        // Check if agent reached the goal
+        if (current_pos.Equals(goal_pos))
+        {
+            Debug.Log("Agent reached the goal!");
+            restart_needed = true;
+        }
+        else if (curr_step >= max_steps)
+        {
+            Debug.Log("Max steps exceeded!");
+            restart_needed = true;
+        }
+
+        // Restart the episode
+        if (restart_needed)
+        {
+            curr_step = 0;
+            current_pos = new Tuple<int, int>(0, 0);
+        }
+    }
+
+    Tuple<int, int> ChooseAction()
+    {
+        // Choose action with highest Q-value or explore randomly
+        if (UnityEngine.Random.value < exploration_rate)
+        {
+            return GetRandomAction();
+        }
+        else
+        {
+            return GetBestAction();
+        }
+    }
+
+    Tuple<int, int> GetRandomAction()
+    {
+        // Choose random action among available actions
+        List<string> available_actions = GetAvailableActions(current_pos);
+        int random_index = UnityEngine.Random.Range(0, available_actions.Count);
+        return GetNextPosition(current_pos, available_actions[random_index]);
+    }
+
+    Tuple<int, int> GetBestAction()
+    {
+        // Choose action with highest Q-value among available actions
+        List<string> available_actions =
+        GetAvailableActions(current_pos);
+        string best_action = "";
+        float best_q_value = float.NegativeInfinity;
+        foreach (string action in available_actions)
+        {
+            Tuple<int, int> next_pos = GetNextPosition(current_pos, action);
+            float q_value = GetQValue(current_pos, action);
+            if (q_value > best_q_value)
+            {
+                best_action = action;
+                best_q_value = q_value;
+            }
+        }
+        return GetNextPosition(current_pos, best_action);
+    }
+
+    List<string> GetAvailableActions(Tuple<int, int> pos)
+    {
+        // Get list of available actions at given position
+        List<string> available_actions = new List<string>();
+        foreach (string action in actions)
+        {
+            Tuple<int, int> next_pos = GetNextPosition(pos, action);
+            if (IsValidPosition(next_pos))
+            {
+                available_actions.Add(action);
+            }
+        }
+        return available_actions;
+    }
+
+    bool IsValidPosition(Tuple<int, int> pos)
+    {
+        // Check if position is within bounds and not an obstacle
+        if (pos.Item1 < 0 || pos.Item1 >= nx || pos.Item2 < 0 || pos.Item2 >= ny)
+        {
+            return false;
+        }
+        foreach (Tuple<int, int> obstacle in obstacles)
+        {
+            if (pos.Equals(obstacle))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    Tuple<int, int> GetNextPosition(Tuple<int, int> pos, string action)
+    {
+        // Get next position given current position and action
+        int x = pos.Item1;
+        int y = pos.Item2;
+        if (action == "up")
+        {
+            y += 1;
+        }
+        else if (action == "down")
+        {
+            y -= 1;
+        }
+        else if (action == "left")
+        {
+            x -= 1;
+        }
+        else if (action == "right")
+        {
+            x += 1;
+        }
+        return new Tuple<int, int>(x, y);
+    }
+
+    float GetQValue(Tuple<int, int> state, string action)
+    {
+        // Get Q-value for given state-action pair
+        Dictionary<string, float> state_actions = q_table[state];
+        return state_actions[action];
+    }
+
+    float GetReward(Tuple<int, int> next_pos)
+    {
+        // Get reward for transitioning from current position to next position
+        if (next_pos.Equals(goal_pos))
+        {
+            return 100.0f;
+        }
+        else if (obstacles.Contains(next_pos))
+        {
+            return -100.0f;
+        }
+        else
+        {
+            return -1.0f;
+        }
+    }
+
+    void UpdateQTable(Tuple<int, int> state, Tuple<int, int> next_state, float reward)
+    {
+        // Update Q-table using Q-learning algorithm
+        float q_value = GetQValue(state, MagicStringConversion(state, GetBestAction()));
+        float max_q_value = GetQValue(next_state, MagicStringConversion(next_state, GetBestAction()));
+        float updated_q_value = q_value + learning_rate * (reward + discount_factor * max_q_value - q_value);
+        q_table[state][MagicStringConversion(state, GetBestAction())] = updated_q_value;
+    }
+
+    // Uhhhhh honestly I dunno man.
+    string MagicStringConversion(Tuple<int, int> curr, Tuple<int, int> best_action) {
+        int sx = curr.Item1;
+        int sz = curr.Item2;
+
+        int tx = best_action.Item1;
+        int tz = best_action.Item2;
+
+        if (tx < sx)
+        {
+            return "left";
+        }
+        else if (tx > sx)
+        {
+            return "right";
+        }
+        else if (tz < sz)
+        {
+            return "down";
+        }
+        else if (tz > sz)
+        {
+            return "up";
+        }
+
+        return "panic";  // this will break things
+    }
+}
